c69d6853084458ac8f6ace1437cd1b8c5a7d1497
franciscojoel <joelcevallos363@gmail.com>
2022-05-04 21:44:17 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/* const API_URL = '/data/products.json';
+
+document.addEventListener('DOMContentLoaded', () => {
+    getData();
+});
+
+const getData = async () => {
+    try {
+        const res = await fetch(API_URL);
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+} */ 
