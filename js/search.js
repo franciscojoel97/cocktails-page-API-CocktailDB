4fdeffc00e97ad88c6a16d12ffc8f0ab5fd0ff7f
franciscojoel <joelcevallos363@gmail.com>
2022-05-16 21:59:10 -0500

    search building v2

--- js/search.js
+++ js/search.js
@@ -6,3 +6,7 @@ const URL = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 const form = getData('.form-search'); //Guardar variable de formulario
 const input = getData('[name="cocktail"]'); //Guardar variable del input del form
+
+form.addEventListener('keyup', (e) => {
+    e.preventDefault();
+});
