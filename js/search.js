5c193f3ee8fb55360c581629ce838d3b29c7de0f
franciscojoel <joelcevallos363@gmail.com>
2022-05-25 21:43:36 -0500

    search correciones

--- js/search.js
+++ js/search.js
@@ -1,10 +1,10 @@
 import getElement from './getElement.js';
 import getData from './loadDATA.js';
 
-const URL = 'www.thecocktaildb.com/api/json/v1/1/search.php?s='; //Busqueda por URL de API
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='; //Busqueda por URL de API
 
 const form = getElement('.form'); //Guardar variable de formulario
-const input = getElement('[name="drinks"]'); //Guardar variable del input del form
+const input = getElement('[name="cocktail"]'); //Guardar variable del input del form
 
 form.addEventListener('keyup', function (e) {
     e.preventDefault();
