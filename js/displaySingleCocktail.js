7cda4e48aaf30417cf51e2129623a08f3caaa241
franciscojoel <joelcevallos363@gmail.com>
2022-06-23 21:00:37 -0500

    building

--- js/displaySingleCocktail.js
+++ js/displaySingleCocktail.js
@@ -1,5 +1,10 @@
-const displayCocktail = (cocktail) => {
-    console.log(cocktail);
+import { hideLoading } from './spinnerLoading.js'; //importar ocultar loading
+import getElement from './getElement.js';
+
+const displayCocktail = (data) => {
+    hideLoading();
+    
+    const drink = data.drinks[0];
 }
 
 export default displayCocktail;
