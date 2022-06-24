c7e6cbbf2c0a89b3c86bdaddb9566bb10410a247
franciscojoel <joelcevallos363@gmail.com>
2022-06-23 21:19:54 -0500

    vodka barato

--- js/displaySingleCocktail.js
+++ js/displaySingleCocktail.js
@@ -5,6 +5,23 @@ const displayCocktail = (data) => {
     hideLoading();
     
     const drink = data.drinks[0];
+    const {strDrinkThumb: image, strDrink: name, strInstructions: desc} = drink;
+    //Listado de ingredientes
+    const listIngredients = [
+        drink.strIngredient1,
+        drink.strIngredient2,
+        drink.strIngredient3,
+        drink.strIngredient4,
+        drink.strIngredient5
+    ];
+
+    //Obtenemos la referencia de las clases del documento HTML
+    const img = getElement('.cocktail-img');
+    const cocktailName = getElement('.cocktail-name');
+    const description = getElement('.cocktail-desc');
+    const ingredientes = getElement('.cocktail-ingredients');
+
+    console.log(drink, listIngredients);
 }
 
 export default displayCocktail;
