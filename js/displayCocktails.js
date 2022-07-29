2261d8c69cb0f49cd5a3481249dcd548b144f743
franciscojoel <joelcevallos363@gmail.com>
2022-07-28 22:07:56 -0500

    ingredients

--- js/displayCocktails.js
+++ js/displayCocktails.js
@@ -18,11 +18,20 @@ const displayCocktails = ({ drinks }) => {
         //Cocktail object
         const {idDrink: id, strDrink: name, strDrinkThumb: image, strCategory: category} = cocktail;
 
+        const listIngredients = [
+            cocktail.strIngredient1,
+            cocktail.strIngredient2,
+            cocktail.strIngredient3,
+            cocktail.strIngredient4,
+            cocktail.strIngredient5
+        ];
+
         return `<a href="cocktail.html" class="a-cocktails">
                     <article class="ccocktails" data-id="${id}">
                         <img src="${image}" alt="${name}">
                         <h3>${name}</h3>
                         <p><span>Categoría:</span> ${category}</p>
+                        <p class="cocktail-ingredients"><span>Ingrediente: </span>${listIngredients[0]}, ${listIngredients[1]}, ${listIngredients[2]}</p>
                     </article>
                 </a>`
     }).join('');
